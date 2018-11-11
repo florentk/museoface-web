6180851e143eb308a561e226fea44b091cc4dedb
Florent Kaisser <florent@kaisser.name>
2018-11-11 03:33:09 +0100

    add upload file feature

--- htdocs/application/controllers/Welcome.php
+++ htdocs/application/controllers/Welcome.php
@@ -114,4 +114,53 @@ class Welcome extends CI_Controller {
 	 # $this->VisiteurModel->setQueteId($id,2);
 	}
 	
+	public function send_image()
+	{
+	
+	  $email=($this->input->post('email'));
+	  $v = $this->VisiteurModel->getVisiteurFromMail($email);
+    
+	  if($v)
+	    $file_id = $v->visiteur_id;
+	  else{
+	    $file_id = 0;
+	    $uploadOk = 0;
+	  }
+	  
+    $target_dir = "uploads/";
+    $target_file = $target_dir . "result_" .$file_id.".jpg";
+    $uploadOk = 1;
+    
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["result"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+    // Check file size
+    if ($_FILES["result"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["result"]["tmp_name"], $target_file)) {
+            echo "The file has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }	
+
+	
+	}	
+	
+	
 }

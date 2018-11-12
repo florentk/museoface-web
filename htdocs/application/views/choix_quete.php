ec2d569ec126e0d81eb2b97e268a93cf46bf09b2
Florent Kaisser <florent@kaisser.name>
2018-11-12 22:22:04 +0100

    remise en forme

--- htdocs/application/views/choix_quete.php
+++ htdocs/application/views/choix_quete.php
@@ -16,15 +16,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 				  
 		  <div id = "list">
-				<div><a href= <?php echo base_url("index.php/welcome/quete?quete=1"); ?> "  ><img src="<?php echo base_url("img/jesus.png"); ?>">  Vous, ici!?  </img> </a></div>
-				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=2"); ?> "  ><img src="<?php echo base_url("img/femmerouge.png"); ?>"> Parce qu’elle le vaut bien!  </img></a></div>
-				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=3"); ?> "  ><img src="<?php echo base_url("img/femmepied.png"); ?>"> Ciel ! Mon mari !  </img></a></div>
-				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=4"); ?> "  ><img src="<?php echo base_url("img/femmeblanche.png"); ?>"> C’est moi la plus belle! </img></a></div>
-				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=5"); ?> "  ><img src="<?php echo base_url("img/hommechapeau.png"); ?>"> Je pense donc je suis!  </img></a></div>
-				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=6"); ?> "  ><img src="<?php echo base_url("img/jeunehomme.png"); ?>"> La flemme  </img></a></div>
-				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=7"); ?> "  ><img src="<?php echo base_url("img/vieillefemme.png"); ?>"> Il fait froid ici !  </img></a></div>
-				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=8"); ?> "  ><img src="<?php echo base_url("img/hommeregardhaut.png"); ?>"> Aaah, si vous saviez!  </img></a></div>
-				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=9"); ?> "  ><img src="<?php echo base_url("img/hommecanne.png"); ?>"> Quel homme ! </img></a></div>
+				<div><a href= <?php echo base_url("index.php/welcome/quete?quete=1"); ?> "  ><img src="<?php echo base_url("img/jesus.png"); ?>"></img></a><br/> Vous, ici!?  </div>
+				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=2"); ?> "  ><img src="<?php echo base_url("img/femmerouge.png"); ?>"></img></a><br/>Parce qu’elle le vaut bien!  </div>
+				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=3"); ?> "  ><img src="<?php echo base_url("img/femmepied.png"); ?>"></img></a><br/>Ciel ! Mon mari !  </div>
+				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=4"); ?> "  ><img src="<?php echo base_url("img/femmeblanche.png"); ?>"></img></a><br/>C’est moi la plus belle! </div>
+				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=5"); ?> "  ><img src="<?php echo base_url("img/hommechapeau.png"); ?>"></img></a><br/>Je pense donc je suis!  </div>
+				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=6"); ?> "  ><img src="<?php echo base_url("img/jeunehomme.png"); ?>"></img></a><br/>La flemme  </img></a></div>
+				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=7"); ?> "  ><img src="<?php echo base_url("img/vieillefemme.png"); ?>"></img></a><br/>Il fait froid ici !  </div>
+				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=8"); ?> "  ><img src="<?php echo base_url("img/hommeregardhaut.png"); ?>"></img></a><br/>Aaah, si vous saviez!  </div>
+				<div><a href=" <?php echo base_url("index.php/welcome/quete?quete=9"); ?> "  ><img src="<?php echo base_url("img/hommecanne.png"); ?>"></img></a><br/>Quel homme ! </div>
 		  </div>
 
 	</div>
